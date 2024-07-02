f0ad94c6c0c39cd879953b9fce3d8fd2b121eed6
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-07-02 16:03:04 +0545

    :li

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/Header'
+import DisplaySec from './pages/home/DisplaySec'
+import Tech from './pages/home/Tech'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <DisplaySec />
+      <Tech />
+
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='bg-black text-white flex items-end justify-between px-4 py-2'>
+
+      <h1 className='text-3xl'>Web Tail</h1>
+
+      <nav className=''>
+        <a href="" className='hover:bg-white hover:text-black px-2 py-2'>About</a>
+        <a href="" className='hover:bg-white hover:text-black px-2 py-2'>Contact</a>
+
+      </nav>
+
+
+    </header>
+  )
+}
+
+export default Header
