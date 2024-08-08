ea893599d39a974968d13088f8b9c3af4a986971
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-08-08 07:51:45 +0545

    naya kura ho

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { blogSlice } from "../features/blog/blogSlice";
 
 
 
 export const store = configureStore({
   reducer: {
-    [blogSlice.name]: blogSlice.reducer
+
   }
 })
