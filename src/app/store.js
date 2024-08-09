3305da03edcf044a8bf232d133db7e10539f1927
Rabyn900 <82014570+Rabyn-Collab@users.noreply.github.com>
2024-08-09 08:03:32 +0545

    ch

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { productApi } from "../features/product/productApi";
 
 
 
 export const store = configureStore({
   reducer: {
-
-  }
+    [productApi.reducerPath]: productApi.reducer
+  },
+  middleware: (getDefaultMiddlware) => getDefaultMiddlware().concat([
+    productApi.middleware
+  ])
 })
